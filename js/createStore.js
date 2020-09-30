fe6bbe34305e1f66c7c3f44c9106869f23fec0af
chrisb3546 <chrisb3546@yahoo.com>
2020-09-30 15:33:12 -0400

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -1,4 +1,16 @@
-let state;
+let store = createStore(reducer);
+store.dispatch({ type: '@@INIT' });
+
+function createStore() {
+  let state;
+  
+  function dispatch(action){
+    state = reducer(state, action);
+    render();
+  };
+
+  return { dispatch }
+}
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
@@ -10,19 +22,18 @@ function reducer(state = { count: 0 }, action) {
   }
 };
 
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
 
-function render() {
-  let container = document.getElementById('container');
-  container.textContent = state.count;
-};
+
+
+  function render() {
+    let container = document.getElementById('container');
+    container.textContent = store.getState().count;
+  };
+
 
 dispatch({ type: '@@INIT' })
 let button = document.getElementById('button');
 
-button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
+button.addEventListener('click', () => {
+    store.dispatch({ type: 'INCREASE_COUNT' });
 })
