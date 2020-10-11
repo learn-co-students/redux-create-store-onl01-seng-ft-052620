bb83945c9845f1a8fcbd80801d16c6c7d9273aa5
Kristin239 <hkristin@andrew.cmu.edu>
2020-10-11 18:33:49 -0400

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -1,18 +1,14 @@
-let state;
+function createStore(reducer) {
+  let state;
 
-function reducer(state = { count: 0 }, action) {
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return { count: state.count + 1 };
-
-    default:
-      return state;
-  }
-};
 
 function dispatch(action){
   state = reducer(state, action);
   render();
+};
+
+function getState() {
+  return state;
 };
 
 function render() {
